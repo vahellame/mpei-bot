7da8c130ae922eb5242a428512a88bd1161445b6
Vadim <teanuxx@gmail.com>
2019-12-11 10:23:01 +0300

    fix logs

--- RunMe.sh
+++ RunMe.sh
@@ -1,1 +1,1 @@
-nohup python3 bot_mpei.py > logs.txt 2>&1 &
+nohup python3 bot_mpei.py > /dev/null 2>&1 &
--- bot_mpei.py
+++ bot_mpei.py
@@ -69,75 +69,75 @@ def mpei_bot():
     run_for_all = True
     run_for_admin = False
     while i_feel_pain:
-        # try:
-        while run_for_all:
-            messages = vk.method("messages.getConversations", {"offset": 0, "count": 200, "filter": "unanswered"})
-            if messages["count"] > 0:
-                user_id = messages['items'][0]['last_message']['peer_id']
-                body = messages['items'][0]['last_message']['text']
-                check_id(user_id, main_db_path)
-# JUST DIALOG ==========================================================================================================
-                if user_id != vk_id_admin and check_just_dialog(user_id, main_db_path):
-                    if body.lower() == "привет":
-                        send_message(user_id, "Добро пожаловать. Снова.")
-                    elif body.lower() == "записаться на консультацию" or body.lower() == "отмена":
-                        set_param(user_id, "in_add_meeting", 1, main_db_path)
+        try:
+            while run_for_all:
+                messages = vk.method("messages.getConversations", {"offset": 0, "count": 200, "filter": "unanswered"})
+                if messages["count"] > 0:
+                    user_id = messages['items'][0]['last_message']['peer_id']
+                    body = messages['items'][0]['last_message']['text']
+                    check_id(user_id, main_db_path)
+    # JUST DIALOG ==========================================================================================================
+                    if user_id != vk_id_admin and check_just_dialog(user_id, main_db_path):
+                        if body.lower() == "привет":
+                            send_message(user_id, "Добро пожаловать. Снова.")
+                        elif body.lower() == "записаться на консультацию" or body.lower() == "отмена":
+                            set_param(user_id, "in_add_meeting", 1, main_db_path)
+                            response = add_meeting_request(user_id, body)
+                            if response['exit']:
+                                back_just_dialog(user_id, main_db_path)
+                            send_message(user_id, response['message'],
+                                         keyboard=response['keyboard'],
+                                         attachments=response['keyboard'])
+                        else:
+                            send_message(user_id, "Что дальше?", keyboard=kb_main)
+    # IN ADD MEETING - REQUEST =============================================================================================
+                    elif user_id != vk_id_admin and check_param(user_id, "in_add_meeting", main_db_path):
                         response = add_meeting_request(user_id, body)
                         if response['exit']:
                             back_just_dialog(user_id, main_db_path)
                         send_message(user_id, response['message'],
                                      keyboard=response['keyboard'],
-                                     attachments=response['keyboard'])
-                    else:
-                        send_message(user_id, "Что дальше?", keyboard=kb_main)
-# IN ADD MEETING - REQUEST =============================================================================================
-                elif user_id != vk_id_admin and check_param(user_id, "in_add_meeting", main_db_path):
-                    response = add_meeting_request(user_id, body)
-                    if response['exit']:
-                        back_just_dialog(user_id, main_db_path)
-                    send_message(user_id, response['message'],
-                                 keyboard=response['keyboard'],
-                                 attachments=response['attachments'])
-# IN ADD MEETING - RESPONSE ============================================================================================
-                elif user_id == vk_id_admin:
-                    add_meeting_response(body)
-# STOPPING BOT =========================================================================================================
-                elif user_id == vk_id_admin and body.lower() == vk_key_admin:
-                    run_for_all = False
-                    run_for_admin = True
-                    send_message(vk_id_admin, "Бот остановлен")
-# RUN ONLY FOR ADMIN ===================================================================================================
-        while run_for_admin:
-            messages = vk.method("messages.getConversations", {"offset": 0, "count": 200, "filter": "unanswered"})
-            if messages["count"] >= 1:
-                user_id = messages['items'][0]['last_message']['peer_id']
-                body = messages['items'][0]['last_message']['text']
-                if body.lower() == vk_key_admin and user_id == vk_id_admin:
-                    run_for_all = True
-                    run_for_admin = False
-                    send_message(user_id, "Бот запущен", None)
-    # except Exception as ex:
-    #     exc_type, exc_obj, tb = sys.exc_info()
-    #     print(tb)
-    #     f = tb.tb_frame
-    #     lineno = tb.tb_lineno
-    #     filename = f.f_code.co_filename
-    #     linecache.checkcache(filename)
-    #     line = linecache.getline(filename, lineno, f.f_globals)
-    #     log = 'Error in {}, line {} "{}": {}\n'.format(filename, lineno, line.strip(), ex)
-    #     log_file = open(home_path + "logs.txt", "a")
-    #     if run_for_admin:
-    #         log = "WARNING. ERROR IN ADMIN MODE. " + log
-    #     log_file.write(log)
-    #     log_file.close()
-    #     time.sleep(1)
-    #     if os.stat('logs.txt').st_size > 309600:
-    #         exit()
-    #     try:
-    #         vk._auth_token()
-    #     except Exception as ex:
-    #         del ex
-    #         time.sleep(1)
+                                     attachments=response['attachments'])
+    # IN ADD MEETING - RESPONSE ============================================================================================
+                    elif user_id == vk_id_admin:
+                        add_meeting_response(body)
+    # STOPPING BOT =========================================================================================================
+                    elif user_id == vk_id_admin and body.lower() == vk_key_admin:
+                        run_for_all = False
+                        run_for_admin = True
+                        send_message(vk_id_admin, "Бот остановлен")
+    # RUN ONLY FOR ADMIN ===================================================================================================
+            while run_for_admin:
+                messages = vk.method("messages.getConversations", {"offset": 0, "count": 200, "filter": "unanswered"})
+                if messages["count"] >= 1:
+                    user_id = messages['items'][0]['last_message']['peer_id']
+                    body = messages['items'][0]['last_message']['text']
+                    if body.lower() == vk_key_admin and user_id == vk_id_admin:
+                        run_for_all = True
+                        run_for_admin = False
+                        send_message(user_id, "Бот запущен", None)
+        except Exception as ex:
+            exc_type, exc_obj, tb = sys.exc_info()
+            print(tb)
+            f = tb.tb_frame
+            lineno = tb.tb_lineno
+            filename = f.f_code.co_filename
+            linecache.checkcache(filename)
+            line = linecache.getline(filename, lineno, f.f_globals)
+            log = 'Error in {}, line {} "{}": {}\n'.format(filename, lineno, line.strip(), ex)
+            log_file = open(home_path + "logs.txt", "a")
+            if run_for_admin:
+                log = "WARNING. ERROR IN ADMIN MODE. " + log
+            log_file.write(log)
+            log_file.close()
+            time.sleep(1)
+            if os.stat('logs.txt').st_size > 309600:
+                exit()
+            try:
+                vk._auth_token()
+            except Exception as ex:
+                del ex
+                time.sleep(1)
 
 
 thread1 = Thread(target=mpei_bot)
